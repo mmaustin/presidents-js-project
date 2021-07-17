7bc9bbc05a05e3ed41958e4a5539d7d26138f953
McCray <tubach75@hotmail.com>
2021-07-17 12:21:31 -0400

    added script files to html file, added calls in js file

---
+++ frontend/index.js
@@ -0,0 +1,14 @@
+const base_url = "http://127.0.0.1:3000"
+const presidentService = new PresidentService(base_url)
+presidentService.getPresidents()
+President.presidentialForm()
+
+//const achievementService = new AchievementService(base_url)
+//achievementService.getAchievements()
+
+President.presidentForm.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event){
+  event.preventDefault
+  presidentService.createPresident()
+}
