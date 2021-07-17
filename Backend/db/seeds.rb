42e123f509b645e1b4d3c8fd99676dde6a1cd244
McCray <tubach75@hotmail.com>
2021-07-17 10:41:32 -0400

    added seed data

--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+President.create(name: "Abraham Lincoln")
+President.create(name: "Franklin Roosevelt")
+President.create(name: "Lyndon Johnson")
+
+Achievement.create([
+    {title: "Emancipation Proclamation", year: 1863, president_id: 1},
+    {title: "WPA", year: 1935, president_id: 2},
+    {title: "Civil Rights Act", year: 1964, president_id: 3}
+])
