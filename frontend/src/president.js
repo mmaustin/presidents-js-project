a5a756f018bbd0004161eb1e6cf1eb6b69605321
McCray <tubach75@hotmail.com>
2021-07-17 11:34:54 -0400

    added method that dynamically creates html objects and appends to the dom

--- frontend/src/president.js
+++ frontend/src/president.js
@@ -12,6 +12,23 @@ class President {
 
     }
 
+    appendToDom(){
+        let string = ""
+        this.achievements.forEach(a => {
+            string += `<li id="list-item" data-achievement-id="${a.id}">${a.title}
+            <button type="button" id="a-delete-bttn">Delete</button> </li>`
+        })
+        President.presidentsContainer.innerHTML += `
+        <div id="${this.id}" class="border"><p>${this.name}</p><p>Achievements</p>
+        <ul>${string}</ul><form id="achievement-form">
+        Title: <input type="text" id="title"><br>
+        Year: <input type="number" id="year"><br>
+        <input type="submit" id="achievement"><br>
+        </form>
+        <button type="button" id="delete-bttn">Delete</button>
+        </div>
+        `
 
+    }
 
 }
