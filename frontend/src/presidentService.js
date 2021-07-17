36bb752879fb659548aa3ed2891b620828fa0e0a
McCray <tubach75@hotmail.com>
2021-07-17 11:51:21 -0400

    create method to get objects from backend, turn them into js objects, and append objects to dom

--- frontend/src/presidentService.js
+++ frontend/src/presidentService.js
@@ -3,6 +3,15 @@ class PresidentService {
         this.endpoint = endpoint
     }
 
+    getPresidents(){
+        fetch(`${this.endpoint}/presidents`)
+        .then(resp => resp.json())
+        .then(presidents => {
+            for(const prez of presidents){
+                const p = new President(prez)
+                p.appendToDom()
+            }
+        })
+    }
 
-    
 }
