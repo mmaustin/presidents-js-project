46ffe4471e4970cc69535e4a86e394e5b26799cc
McCray <tubach75@hotmail.com>
2021-07-18 13:17:10 -0400

    added additional spacing stylings

--- frontend/src/president.js
+++ frontend/src/president.js
@@ -21,6 +21,7 @@ class President {
         President.presidentsContainer.innerHTML += `
         <div id="${this.id}" class="border"><p id="p-one">${this.name}</p><p id="p-two">Achievements:</p>
         <ul>${string}</ul><form id="achievement-form">
+        Add an achievement:<br>
         Title: <input type="text" id="title"><br>
         Year: <input type="number" id="year"><br><br>
         <input type="submit" id="achievement"><br>
@@ -35,8 +36,8 @@ class President {
         President.presidentForm.innerHTML += `
         <form id="president-form-inner">
             Add A New President Here:<br><br>
-            Name: <input type="text" id="name"><br>
-            Submit: <input type="submit" id="create">
+            Name: <input type="text" id="name">
+            <input type="submit" id="create">
         </form>
         `
     }
