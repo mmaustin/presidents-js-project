d00750fc528adf8aca8e596c24b5eab36bd3a96e
McCray <tubach75@hotmail.com>
2021-07-18 11:04:50 -0400

    added spacing and font sizing and colors

--- frontend/src/president.js
+++ frontend/src/president.js
@@ -19,13 +19,13 @@ class President {
             <button type="button" id="a-delete-bttn">Delete</button> </li>`
         })
         President.presidentsContainer.innerHTML += `
-        <div id="${this.id}" class="border"><p>${this.name}</p><p>Achievements</p>
+        <div id="${this.id}" class="border"><p id="p-one">${this.name}</p><p id="p-two">Achievements:</p>
         <ul>${string}</ul><form id="achievement-form">
         Title: <input type="text" id="title"><br>
-        Year: <input type="number" id="year"><br>
+        Year: <input type="number" id="year"><br><br>
         <input type="submit" id="achievement"><br>
         </form>
-        <button type="button" id="delete-bttn">Delete</button>
+        Remove President: <button type="button" id="delete-bttn">Delete</button>
         </div>
         `
 
@@ -34,7 +34,7 @@ class President {
     static presidentialForm(){
         President.presidentForm.innerHTML += `
         <form id="president-form-inner">
-            New President<br>
+            Add A New President Here:<br><br>
             Name: <input type="text" id="name"><br>
             Submit: <input type="submit" id="create">
         </form>
